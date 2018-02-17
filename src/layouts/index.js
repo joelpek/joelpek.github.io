1672bdaa2f6dfe055eff681ecb50ce21b69aebe1
Joel Pekari <pekarijoel@gmail.com>
2018-02-17 13:20:19 +0200

    update page title

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,7 +8,7 @@ import './index.scss'
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Joel Pekari | UX, Web Design & Development"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
